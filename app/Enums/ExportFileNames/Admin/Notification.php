9d7d2beb56ff6fa2431879c8ccbed816312a55c7
WorkSaar Expert <paswan.narayan@gmail.com>
2026-01-03 13:09:43 +0530

    good

--- app/Enums/ExportFileNames/Admin/Notification.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Enums\ExportFileNames\Admin;
-
-enum Notification
-{
-    const EXPORT_CSV = 'Notification.csv';
-    const EXPORT_XLSX = 'Notification.xlsx';
-}
--- routes/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', 'UpdateController@update_software_index')->name('index');
-Route::post('update-system', 'UpdateController@update_software')->name('update-system');
-
-Route::fallback(function () {
-    return redirect('/');
-});
