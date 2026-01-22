91cf8e139708db48a5ce8b52cb8483dff2e5e47b
WorkSaar Expert <paswan.narayan@gmail.com>
2026-01-22 14:37:51 +0530

    payments

--- app/Traits/Payment.php
+++ app/Traits/Payment.php
@@ -73,6 +73,9 @@ trait Payment
             'phonepe' => 'payment/phonepe/pay',
             'cashfree' => 'payment/cashfree/pay',
             'instamojo' => 'payment/instamojo/pay',
+            //worksaar start
+            'ndasenda' => 'payment/ndasenda/pay',
+            //worksaar end
         ];
         if (array_key_exists($payment->payment_method, $routes)) {
             return url("{$routes[$payment->payment_method]}/?payment_id={$payment->id}");
